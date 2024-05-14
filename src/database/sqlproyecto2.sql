294ce4c0d1dd4564e41592d9a4158fd26f03e324
millerbermeo <millerbermeo30@gmail.com>
2024-05-13 21:46:05 -0500

    base datos

--- src/database/sqlproyecto2.sql
+++ src/database/sqlproyecto2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 14-05-2024 a las 04:28:28
+-- Tiempo de generación: 14-05-2024 a las 04:42:26
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
