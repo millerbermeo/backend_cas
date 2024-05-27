ae0045b03ed47592352d01d55b9bca460e29d090
millerbermeo <millerbermeo30@gmail.com>
2024-05-27 15:45:25 -0500

    commit controlador de garficos

--- src/controllers/graficos.controller.js
+++ src/controllers/graficos.controller.js
@@ -99,3 +99,35 @@ export const listarAlmCantidad = async (req, res) => {
         return res.status(500).json({ 'message': 'Error: ' + e });
     }
 }
+
+
+export const selectX = async (req, res) => {
+    try {
+        const { rol } = req.user;
+
+        if (rol === 'administrador') {
+            const query = `
+                SELECT r.unidad_medida, SUM(cantidad) as total 
+                FROM residuos r 
+                GROUP BY unidad_medida 
+                ORDER BY total DESC
+             
+            `;
+
+      
+
+            const [result] = await pool.query(query);
+
+            if (result.length > 0) {
+                return res.status(200).json(result);
+            } else {
+                return res.status(404).json({ message: 'No se encontraron registros' });
+            }
+        } else {
+            return res.status(403).json({ message: 'Error: usuario no autorizado' });
+        }
+    } catch (e) {
+        return res.status(500).json({ message: 'Error: ' + e });
+    }
+};
+
--- src/routers/graficos.router.js
+++ src/routers/graficos.router.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { validarToken } from "../controllers/validator.controller.js";
-import { listarAlmCantidad, listarMovMesY, residuoListarTipo } from "../controllers/graficos.controller.js";
+import { listarAlmCantidad, listarMovMesY, residuoListarTipo, selectX } from "../controllers/graficos.controller.js";
 
 const router = Router()
 
 router.post('/listarMovimientosMes', validarToken, listarMovMesY)
 router.get('/listarTipos', validarToken, residuoListarTipo)
 router.get('/listarAlm', validarToken, listarAlmCantidad)
+router.get('/listarr', validarToken, selectX)
 
 export default router
