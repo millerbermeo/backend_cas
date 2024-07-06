79f0ae8e0560807a7a7cae786cd62ee692fcaf60
millerbermeo <millerbermeo30@gmail.com>
2024-07-06 15:23:52 -0500

    commit dev

--- src/controllers/emailService.js
+++ src/controllers/emailService.js
@@ -5,14 +5,16 @@ export const sendPasswordResetEmail = async (email, token) => {
   const transporter = nodemailer.createTransport({
     service: 'Gmail', // O el servicio que estés usando
     auth: {
-      user: 'millerrivera2002@gmail.com',
-      pass: 'esvk ivyz kwwh kfti', 
+      user: 'centrodeacopio148@gmail.com',
+      pass: 'jlar fbgg lbff rofn', 
     }
   });
 
-  const resetLink = `http://localhost:5173/reset-password?token=${token}`;
+  let baseUrl = "http://localhost:5173"
+
+  const resetLink = `${baseUrl}/reset-password?token=${token}`;
   const mailOptions = {
-    from: 'tu_email@gmail.com',
+    from: 'centrodeacopio148@gmail.com',
     to: email,
     subject: 'Restablecimiento de contraseña',
     text: `Utiliza este enlace para restablecer tu contraseña: ${resetLink}`
